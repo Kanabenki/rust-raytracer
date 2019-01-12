1fe652ea3e67a8835dce372342b022c2523093be
Kanabenki <18357657+Kanabenki@users.noreply.github.com>
2019-01-12 14:25:52 +0100

    Add Ray and sphere drawing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::ops;
 
+#[derive(Debug, Clone, Copy)]
 struct Vec3 {
     e : [f64; 3]
 }
@@ -8,6 +9,10 @@ impl Vec3 {
 
     pub fn new(e1: f64, e2: f64, e3: f64) -> Vec3 {
         Vec3 {e: [e1, e2, e3]}
+    }
+
+    pub fn one() -> Vec3 {
+        Vec3 {e: [1.0, 1.0, 1.0]}
     }
 
     pub fn x(&self) -> f64 { self.e[0] }
@@ -27,6 +32,18 @@ impl Vec3 {
 
     pub fn normalized(&self) -> Vec3 {
         self / self.length()
+    }
+
+    pub fn dot(&self, v: &Vec3) -> f64 {
+        self.e[0] * v.e[0] + self.e[1] * v.e[1] + self.e[2] * v.e[2]
+    }
+
+    pub fn cross(&self, v: &Vec3) -> Vec3 {
+        Vec3 { e: [
+            self.e[1] * v.e[2] - self.e[2] * v.e[1],
+            - (self.e[0] * v.e[2] - self.e[2] * v.e[0]),
+            self.e[0] * v.e[1] - self.e[1] * v.e[0]
+            ]}
     }
 }
 
@@ -72,13 +89,53 @@ impl ops::Div<f64> for &Vec3 {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+struct Ray {
+    pub origin: Vec3,
+    pub direction: Vec3
+}
+
+impl Ray {
+    pub fn new(origin: Vec3, direction: Vec3) -> Ray {
+        Ray {origin, direction}
+    }
+
+    pub fn point(self, t: f64) -> Vec3 {
+        self.origin + t * self.direction
+    }
+}
+
+fn color(ray: &Ray) -> Vec3 {
+    if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, &ray) {
+        return Vec3::new(1.0, 0.0, 0.0);
+    }
+    let t = 0.5 * (ray.direction.normalized().y() + 1.0);
+    (1.0 - t) * Vec3::one() + t * Vec3::new(0.5, 0.7, 1.0)
+}
+
+fn hit_sphere(center: &Vec3, radius: f64, ray: &Ray) -> bool {
+    let oc = ray.origin - *center;
+    let a = ray.direction.dot(&ray.direction);
+    let b = 2.0 * oc.dot(&ray.direction);
+    let c = oc.dot(&oc) - radius.powi(2);
+    let disc = b*b - 4.0*a*c;
+    disc > 0.0
+}
+
 fn main() {
     let nx = 200;
     let ny = 100;
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let origin = Vec3::new(0.0, 0.0, 0.0);
     print!("P3\n{} {}\n255\n", nx, ny);
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let col = Vec3::new(i as f64 / nx as f64, j as f64 / ny as f64, 0.2) * 255.99;
+            let u = i as f64 / nx as f64;
+            let v = j as f64 / ny as f64;
+            let ray = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
+            let col = color(&ray) * 255.99;
             println!("{} {} {}", col.r() as u64, col.g() as u64, col.b() as u64);
         }
     }
